937219b613f8bc2e1cfe7c6884ab8e4db79c05d4
luan <konnichiwapp@gmail.com>
2013-05-11 15:13:11 +0700

    upo

---
+++ TabbarTable/Class/OtherView/CellForModifyMenu/TBModifyMenuCell.m
@@ -0,0 +1,29 @@
+//
+//  TBModifyMenuCell.m
+//  TabbarTable
+//
+//  Created by luan on 5/11/13.
+//  Copyright (c) 2013 Luan. All rights reserved.
+//
+
+#import "TBModifyMenuCell.h"
+
+@implementation TBModifyMenuCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
