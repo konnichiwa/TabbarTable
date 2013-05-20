cbb28cc9708ad098833870fe275b265363b2bc99
luan <konnichiwapp@gmail.com>
2013-05-21 01:02:26 +0700

    rfgt

---
+++ BarPoint/Class/Viewcontroller/NewCHhck/DetailClient.h
@@ -0,0 +1,13 @@
+//
+//  DetailClient.h
+//  BarPoint
+//
+//  Created by luan on 5/21/13.
+//  Copyright (c) 2013 Luan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailClient : UIViewController
+
+@end
---
+++ BarPoint/Class/Viewcontroller/NewCHhck/DetailClient.m
@@ -0,0 +1,38 @@
+//
+//  DetailClient.m
+//  BarPoint
+//
+//  Created by luan on 5/21/13.
+//  Copyright (c) 2013 Luan. All rights reserved.
+//
+
+#import "DetailClient.h"
+
+@interface DetailClient ()
+
+@end
+
+@implementation DetailClient
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
